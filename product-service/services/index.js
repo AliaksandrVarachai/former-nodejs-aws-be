776e54d5b78e7294da3755f4478a09cbda054c8c
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2021-04-26 04:33:17 +0300

    Add count to response

--- product-service/services/index.js
+++ product-service/services/index.js
@@ -2,24 +2,33 @@ import * as dataProvider from '../data-access';
 
 /**
  * Provides list of products.
- * @returns {Promise<Array[{ productId, title, description, price }]>}
+ * @returns {Promise<Array[{ id, title, description, price, count }]>}
  */
 export async function index() {
   const productsList = await dataProvider.index();
-  return productsList.map(({ product_id, title, description, price }) => ({
-    productId: product_id,
+  return productsList.map(({ product_id, title, description, price, count }) => ({
+    id: product_id,
     title,
     description,
     price,
+    count,
   }));
 }
 
 /**
  * Provides a product by its ID or null if there is no such product.
  * @param id {string}
- * @returns {Promise<{ title, description, price }|null>}
+ * @returns {Promise<{ id, title, description, price, count }|null>}
  */
 export async function getProductsById(id) {
   const [product] = await dataProvider.getProductsById(id);
-  return product || null;
+  if (!product) return null;
+  const { product_id, title, description, price, count } = product;
+  return {
+    id: product_id,
+    title,
+    description,
+    price,
+    count,
+  };
 }
